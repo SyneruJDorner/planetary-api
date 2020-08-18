4161e9dd1de3c6e592754a5d73c223fc274154d2
Justindd1994 <justindd1994@gmail.com>
2020-08-18 16:43:29 +0200

    added own api route example.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/super_simple')
+def super_simple():
+    return 'Hello from the Planetary API.'
+
 if __name__ == "__main__":
     app.run()
