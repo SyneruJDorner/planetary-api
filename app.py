83e7db0d52b18d5b9c30fc8c128134370a623f3a
Justindd1994 <justindd1994@gmail.com>
2020-08-18 19:08:50 +0200

    URL parameters

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,13 +6,26 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/super_simple')
 def super_simple():
     return jsonify(message='Hello from the Planetary API.')
 
+
 @app.route('/not_found')
 def not_found():
     return jsonify(message='That resource was not found.'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if (age < 18):
+        return jsonify(mmessage="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(mmessage="Welcome " + name + ", you are old enough.")
+
 
 if __name__ == "__main__":
     app.run()
